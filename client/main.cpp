11a2c51a27874e1993350ee755fa08e32dc22b23
FirasCh3 <firaschebil61@gmail.com>
2026-02-11 21:11:26 +0100

    draw player and display window

--- client/main.cpp
+++ client/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
+
 using namespace std;
+using namespace sf;
 int main() {
-    cout<<"test";
+    RenderWindow window(VideoMode({1920, 1080}), "motion interpolation");
+    RectangleShape player({50, 50});
+    player.setFillColor(Color::White);
+    player.setPosition({10, 20});
+    while (window.isOpen()) {
+        while (optional event = window.pollEvent()) {
+            if (event->is<Event::Closed>()) {
+                window.close();
+            }
+        }
+        window.clear();
+        window.draw(player);
+        window.display();
+    }
     return 0;
 }
